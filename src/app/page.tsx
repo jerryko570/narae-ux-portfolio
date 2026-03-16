22ff0f183310cef60655530bab65cad746122218
jerryko570 <jerry.narae@gmail.com>
2026-03-16 12:41:14 +0900

    ✨Feature: Badge 컴포넌트 구축

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import Badge from '@/components/ui/Badge/Badge'
 export default function Home() {
   return (
     <main>
+      <Badge
+        size="md"
+        label="ggggggggggggggg하하"
+        className="bg-orange-500 text-white"
+      ></Badge>
       <h1 className="text-display font-bold">디자인 포폴입니다</h1>
     </main>
   )
