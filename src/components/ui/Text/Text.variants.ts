be570ebb682188cec36b6313b81035e06b18891f
jerryko570 <jerry.narae@gmail.com>
2026-03-17 21:23:56 +0900

    ⚙️Chore: Prettier Tailwind 자동 정렬 설정 추가및 Text.variant.ts에 테스트 적용

--- src/components/ui/Text/Text.variants.ts
+++ src/components/ui/Text/Text.variants.ts
@@ -3,13 +3,13 @@ import { cva } from 'class-variance-authority'
 export const textVariant = cva('', {
   variants: {
     as: {
-      h1: 'text-5xl font-bold', // 70px
-      h2: 'text-4xl font-bold', // 34px
-      h3: 'text-3xl font-semibold', // 30px
-      h4: 'text-2xl font-semibold', // 26px
-      h5: 'text-xl font-medium', // 22px
-      p: 'text-sm font-normal', // 16px
-      span: 'text-xs font-normal', // 14px
+      h1: 'text-5xl leading-snug font-bold', // 48px
+      h2: 'text-4xl leading-snug font-bold', // 36px
+      h3: 'text-3xl leading-snug font-semibold', // 30px
+      h4: 'text-2xl font-semibold', // 24px
+      h5: 'text-xl font-medium', // 20px
+      p: 'text-sm leading-loose font-normal', // 14px
+      span: 'text-xs font-normal', // 12px
     },
   },
   defaultVariants: {
