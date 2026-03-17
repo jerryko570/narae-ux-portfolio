b8314e3e2dfda88d9f3f1f19ce0997d965d25bd6
jerryko570 <jerry.narae@gmail.com>
2026-03-17 10:40:31 +0900

    ✨Feature: Text 테스트 페이지 임시추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import Badge from '@/components/ui/Badge/Badge'
+import Text from '@/components/ui/Text/Text'
 export default function Home() {
   return (
-    <main>
-      <Badge
-        size="md"
-        label="ggggggggggggggg하하"
-        className="bg-orange-500 text-white"
-      ></Badge>
-      <h1 className="text-display font-bold">디자인 포폴입니다</h1>
+    <main className='lex items-center gap-3 pl-30 pt-30 space-y-6'>
+      <Text as='h1'>h1 사이즈입니다.</Text>
+      <Text as='h2'>h2 사이즈입니다.</Text>
+      <Text as='h3'>h3 사이즈입니다.</Text>
+      <Text as='h4'>h4 사이즈입니다.</Text>
+      <Text as='h5'>h5 사이즈입니다.</Text>
+      <Text as='p'>p 사이즈입니다.</Text>
+      <Text as='span'>span 사이즈입니다.</Text>
     </main>
   )
 }
