c3e196415ef19e3021aaecbb98b9b36caf5e7e00
jerryko570 <jerry.narae@gmail.com>
2026-03-27 14:02:22 +0900

    ✨Feature: StatCard 애니메이션 제어 개선 및 StatSection 컴포넌트 분리

--- src/components/ui/Cards/Card.variants.ts
+++ src/components/ui/Cards/Card.variants.ts
@@ -4,6 +4,7 @@ export const CardVariant = cva('flex w-full rounded-lg p-16', {
   variants: {
     theme: {
       dark: 'bg-gray-800 text-white',
+      gray: 'bg-gray-100 text-black',
       white: 'bg-white text-gray-900',
       lightOrange: 'bg-orange-50 text-gray-900',
       orange: 'bg-orange-500 text-white',
--- src/components/ui/Cards/StatCard.tsx
+++ src/components/ui/Cards/StatCard.tsx
@@ -9,49 +9,48 @@ import CountUp from 'react-countup'
 import { useVisible } from '@/hooks/useVisible'
 import { parseValue } from '@/utils/parseValue'
 
-type StatCardProps = StatCardData & {
+type StatCardProps = Partial<StatCardData> & {
   className?: string
+  animated?: boolean
 } & VariantProps<typeof CardVariant>
 
 export default function StatCard({
   title,
-  data,
+  data = '',
   subtitle,
   description,
-  subdescription,
   theme,
   className,
+  animated = true,
 }: StatCardProps) {
   const { ref, started } = useVisible(0.3)
   const { num, suffix } = parseValue(data)
+  const hasNumber = data?.match(/\d/)
 
   return (
     <div
-      ref={ref}
-      className={cn(
-        CardVariant({ theme }),
-        className,
-        'flex h-full flex-col justify-between'
-      )}
+      ref={animated ? ref : undefined}
+      className={cn(CardVariant({ theme }), className, 'flex flex-col')}
     >
       <Text as='h6' className='text-center'>
         {title}
       </Text>
-      <Text as='h1' className='pt-12 text-center text-orange-500'>
-        {started ? (
-          <CountUp start={0} end={num} duration={1.2} suffix={suffix} />
+      <Text as='h1' className='pt-4 text-center text-orange-500'>
+        {hasNumber ? (
+          animated && started ? (
+            <CountUp start={0} end={num} duration={1.2} suffix={suffix} />
+          ) : (
+            `${num}${suffix}`
+          )
         ) : (
-          `0${suffix}`
+          data
         )}
       </Text>
-      <Text as='h6' className='text-center font-light whitespace-pre-line'>
+      <Text as='h6' className='pt-8 text-center font-light whitespace-pre-line'>
         {subtitle}
       </Text>
       <Text as='p' className='text-center whitespace-pre-line'>
         {description}
-      </Text>
-      <Text as='caption' className='pt-8 text-center text-gray-400'>
-        {subdescription}
       </Text>
     </div>
   )
