a770ab000cdc942c1edd737fd7daaaa6c454c0d9
jerryko570 <jerry.narae@gmail.com>
2026-04-25 18:18:42 +0900

    ♻️Refactor: SmolsFeedSection 컴포넌트 이름 변경 및 UxPrincipleLayout 속성 정리

--- src/components/sections/Smols/SmolsStickerSection.tsx
+++ src/components/sections/Smols/SmolsStickerSection.tsx
@@ -1,12 +1,13 @@
-// 개선과정: 꾸미기 영역
-
 import UxPrincipleLayout from '@/components/common/UxPrincipleLayout'
 import { smols } from '@/data/projects'
 
-export default function SmolsStickerSection() {
+export default function SmolsFeedSection() {
   return (
     <div className='bg-gray-100'>
-      <UxPrincipleLayout uxPrinciple={smols.uxPrinciples[2]} index={1} />
+      <UxPrincipleLayout
+        uxPrinciple={smols.uxPrinciples[2]}
+        showSectionHeader
+      />
     </div>
   )
 }
