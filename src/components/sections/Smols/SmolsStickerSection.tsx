86d469b3f96f31934c8ccecefef037fc22ccd54e
jerryko570 <jerry.narae@gmail.com>
2026-04-25 18:19:28 +0900

    ♻️Refactor: SmolsFeedSection 이름 변경 및 불필요한 속성 제거

--- src/components/sections/Smols/SmolsStickerSection.tsx
+++ src/components/sections/Smols/SmolsStickerSection.tsx
@@ -1,13 +1,12 @@
+// 개선과정: 기록하기 영역
+
 import UxPrincipleLayout from '@/components/common/UxPrincipleLayout'
 import { smols } from '@/data/projects'
 
-export default function SmolsFeedSection() {
+export default function SmolsSolutionSection() {
   return (
     <div className='bg-gray-100'>
-      <UxPrincipleLayout
-        uxPrinciple={smols.uxPrinciples[2]}
-        showSectionHeader
-      />
+      <UxPrincipleLayout uxPrinciple={smols.uxPrinciples[2]} />
     </div>
   )
 }
