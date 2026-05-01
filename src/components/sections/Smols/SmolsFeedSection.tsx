5ff7034c0df2c92631914fb869b7308008aabcb9
jerryko570 <jerry.narae@gmail.com>
2026-05-01 14:11:17 +0900

    ♻️Refactor: Smols 섹션에서 UxPrincipleLayout의 클래스 이름 통일

--- src/components/sections/Smols/SmolsFeedSection.tsx
+++ src/components/sections/Smols/SmolsFeedSection.tsx
@@ -11,6 +11,8 @@ export default function SmolsFeedSection() {
           badge: smols.sections.solution.badge ?? '',
           title: smols.sections.solution.title ?? '',
         }}
+        beforePanelClassName='rounded-3xl bg-white px-8 py-8 ring-1 ring-gray-200'
+        afterPanelClassName='rounded-3xl bg-orange-50 px-8 py-8 ring-1 ring-orange-100'
       />
     </div>
   )
